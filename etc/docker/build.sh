dcf0caada8a340151a450d7330b879874e2aa4c3
Emanuel Borsboom <manny@fpcomplete.com>
2016-02-19 10:52:14 -0800

    Add comments to deploy script wrappers

--- etc/docker/build.sh
+++ etc/docker/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# See description at https://github.com/fpco/devops-helpers#wrappers
 set -xe
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 docker build -t fpco/soh-site-base etc/docker/soh-site-base
--- etc/kubernetes/deploy_rc.sh
+++ etc/kubernetes/deploy_rc.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# See description at https://github.com/fpco/devops-helpers#wrappers
+set -xe
 exec "$(dirname ${BASH_SOURCE[0]})/../common/devops-helpers/kubernetes/deploy_rc_helper.sh" \
      --app "soh-site" \
      --repo fpco/soh-site \
