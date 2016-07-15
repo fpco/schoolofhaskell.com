b3fcaecf295bed29d379bf38eeffb91cc7ec21db
Emanuel Borsboom <manny@fpcomplete.com>
2016-07-14 22:19:37 -0700

    etc: update for new Jenkins server

--- etc/docker/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# See description at https://github.com/fpco/devops-helpers#wrappers
-set -xe
-cd "$(dirname "${BASH_SOURCE[0]}")/../.."
-docker build -t fpco/soh-site-base etc/docker/soh-site-base
-stack image container "$@"
--- etc/kubernetes/deploy_rc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# See description at https://github.com/fpco/devops-helpers#wrappers
-set -xe
-exec "$(dirname ${BASH_SOURCE[0]})/../common/devops-helpers/kubernetes/deploy_rc_helper.sh" \
-     --app "soh-site" \
-     --repo fpco/soh-site \
-     --specdir "$(dirname "${BASH_SOURCE[0]}")" \
-     "$@"
